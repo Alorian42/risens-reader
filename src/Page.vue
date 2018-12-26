f1f3830142f77c55a6184f42338ec7dc0e8a75dd
Alorian <alorian@risens.team>
2018-12-26 22:46:19 +0400

    load chapter, prev&next page

---
+++ src/Page.vue
@@ -0,0 +1,45 @@
+<template>
+  <img :src="src" ref="img">
+</template>
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+    },
+
+  },
+
+  data () {
+    return {
+        
+    }
+  },
+  computed: {
+    
+  },
+  methods: {
+
+  },
+  updated () {
+    
+  },
+  mounted () {
+    this.$refs['img'].onload = () => {
+      console.log(this.$refs['img'].width, this.$refs['img'].height);
+      if (this.$refs['img'].width > this.$refs['img'].height) {
+        this.$refs['img'].style.width = "100%";
+      }
+      else {
+        this.$refs['img'].style.height = "100%";
+      }
+      console.log(this.$refs['img'].width, this.$refs['img'].height);
+    };
+  }
+}
+</script>
+<style lang="scss">
+img {
+  
+}
+</style>
