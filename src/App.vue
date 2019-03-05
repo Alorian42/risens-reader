e21f7e73fcc202d8674b6c68d10ed42470289750
Alorian42 <alorian42@gmail.com>
2019-03-05 23:38:58 +0400

    fix patreon link

--- src/App.vue
+++ src/App.vue
@@ -328,7 +328,7 @@ export default {
           type: 'error',
           title: 'Ошибка!',
           text: 'Скачка доступна только для наших патронов!',
-          footer: '<a href="patreon.risens.team">Поддержите нас на Патреоне...</a>'
+          footer: '<a href="https://patreon.risens.team" target="_blank">Поддержите нас на Патреоне...</a>'
         });
 
         return;
