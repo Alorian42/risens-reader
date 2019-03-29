da044e7d36c77bc6a907c1b589c91b64df55906f
Alorian42 <alorian@risens.team>
2019-03-29 21:16:18 +0400

    fix for scrollbar background

--- src/App.vue
+++ src/App.vue
@@ -435,14 +435,14 @@ export default {
 #app::-webkit-scrollbar-track
 {
 	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
-	background-color: #F5F5F5;
+	background-color: transparent;
 	border-radius: 10px;
 }
 
 #app::-webkit-scrollbar
 {
 	width: 10px;
-	background-color: #F5F5F5;
+	background-color: transparent;
 }
 
 #app::-webkit-scrollbar-thumb
